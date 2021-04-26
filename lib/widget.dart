b67a0d56edc7b3151b756e41f07fdf6682186617
paulosergioduff <paulosergioduff@gmail.com>
2021-04-26 16:04:30 -0300

    Firebase funcionando

---
+++ lib/widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MenuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Xrud Engine'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+  }
+}
